cb028c0301237d7471ce9bdcfd17567c98759944
Changyu-Ryou <56837413+Changyu-Ryou@users.noreply.github.com>
2020-03-03 23:51:23 +0900

    ver 1.1
    WebView 추가
    푸시에 따른 url 링크 이동 추가

--- app/src/main/java/com/DevR/mask/MainActivity.java
+++ app/src/main/java/com/DevR/mask/MainActivity.java
@@ -18,6 +18,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,12 +39,14 @@ import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    static WebView mWebView; // 웹뷰 선언
+    private WebSettings mWebSettings; //웹뷰세팅
     //static String from=MyFirebaseMessagingService.from;
     //static String title=MyFirebaseMessagingService.title;
     //static String body=MyFirebaseMessagingService.body;
     //static Map<String, String> data=MyFirebaseMessagingService.data;
     private FirebaseAnalytics mFirebaseAnalytics;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String id = "id";
         String name = "chan";
-
+        intent = getIntent();
         // 토큰이 등록되는 시점에 호출되는 메소드 입니다.
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,
 
@@ -80,15 +85,37 @@ public class MainActivity extends AppCompatActivity {
                         // Log and toast
                         //String msg = getString(token);
                         Log.d("TAG:", token);
-                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
                     }
                 });
 
         //이렇게 ALL 추가 하면 이 디바이스는 ALL을 구독한다는 얘기가 된다.
         FirebaseMessaging.getInstance().subscribeToTopic("ALL");
 
-       /// createNotification("현재 마스크 재고 알림이 작동중입니다.");
+        /// createNotification("현재 마스크 재고 알림이 작동중입니다.");
         //notifon();
+
+        // 웹뷰 시작
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+        mWebSettings = mWebView.getSettings(); //세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
+        System.out.println("url main="+intent.getStringExtra("url"));
+        if (intent.getStringExtra("url") == null)
+            mWebView.loadUrl("http://www.naver.net"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+        else
+            mWebView.loadUrl(intent.getStringExtra("url"));
 
 
     }
@@ -96,43 +123,66 @@ public class MainActivity extends AppCompatActivity {
     // 서비스로 부터 인텐트를 받았을 때의 처리.
     @Override
     protected void onNewIntent(Intent intent) {
-        println( "onNewIntent 호출됨" );
+        println("onNewIntent 호출됨");
 
         // 인텐트를 받은 경우만, 값을 Activity로 전달하도록 합니다.
-        if( intent != null )
-        {
-            processIntent( intent );
+        if (intent != null) {
+            processIntent(intent);
         }
-
 
         super.onNewIntent(intent);
 
     }
 
 
-
     // 인텐트를 처리하도록 합니다.
-    private void processIntent( Intent intent ){
+    private void processIntent(Intent intent) {
 
         String from = intent.getStringExtra("from");
-        if( from == null )
-        {
+        if (from == null) {
 
             // from 값이 없는 경우, 값을 전달하지 않습니다. (푸쉬 노티 메시지가 아닌것을 판단하고 처리하지 않는듯).
-            Log.d( "TAG", "보낸 곳이 없습니다." );
+            Log.d("TAG", "보낸 곳이 없습니다.");
             return;
 
         }
 
 
+        intent = getIntent();
+        String url = intent.getStringExtra("url");
+        Log.d("TAG URL", "urlurlurlurlurl==." + url);
+        mWebView.loadUrl(url);
+
+        /*
+        TextView aa = findViewById(R.id.text);
+        aa.setText(MyFirebaseMessagingService.from);
+        System.out.println("dataaaaaaa==="+MyFirebaseMessagingService.urlString);
+
+
+        Bundle bundle = new Bundle();
+        bundle.putString("url", MyFirebaseMessagingService.urlString);
+        intent.putExtras(bundle);
+        intent.putExtra("from", from);
+        intent.putExtra("title", MyFirebaseMessagingService.messageTitle);
+        intent.putExtra("body", MyFirebaseMessagingService.messageBody);
+        //intent.putExtra("contents", MyFirebaseMessagingService.contents);
+        intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP
+        );
+
+        startActivity(intent);
         // 메시지를 받은 것우 처리를 합니다.
         Log.d( "TAG", "여기서 메시지 응답 처리를 하면 됩니다." );
-
+*/
 
     }
 
 
+    public void btn(View view) {
+        finish();
+    }
 }
+
 
 /*
     // 서비스로 부터 인텐트를 받았을 때의 처리.
--- app/src/main/java/com/DevR/mask/MyFirebaseMessagingService.java
+++ app/src/main/java/com/DevR/mask/MyFirebaseMessagingService.java
@@ -1,9 +1,16 @@
 package com.DevR.mask;
-import android.app.Service;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -12,6 +19,13 @@ import java.util.Map;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FCM";
+   // static String data;
+    Map<String, String> data;
+    static String from;
+    RemoteMessage remoteMessage1;
+    static String urlString;
+    static String messageBody;
+    static String messageTitle;
 
     public MyFirebaseMessagingService() {
     }
@@ -29,18 +43,61 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-
+        remoteMessage1=remoteMessage;
         // 일단 받은 데이터 중, 내용만 가지고와 출력하는 메소드 입니다. (파이어 베이스 홈페이지에서 보내면 데이터는 값이 없을 수 있습니다.)
-        String from = remoteMessage.getFrom();
-        Log.d(TAG,
-                "title:" + remoteMessage.getNotification().getTitle()
-                        + ", body:" + remoteMessage.getNotification().getBody()
-                        + ", data:" + remoteMessage.getData()
-        );
+        from = remoteMessage.getFrom();
+       // Log.d(TAG,
+      //          "title:" + remoteMessage.getNotification().getTitle()
+       //                 + ", body:" + remoteMessage.getNotification().getBody()
+      //                  + ", data:" + remoteMessage.getData()
+      //  );
+
+        data=remoteMessage.getData();
+        urlString = (String) data.get("url");
+        messageBody =  data.get("body");
+        messageTitle = data.get("title");
+        System.out.println("url===="+urlString);
 
 
-        // 액티비티 쪽으로 메시지를 전달하는 메소드를 호출합니다.
-        sendToActivity(
+      //  String messageBody = remoteMessage.getNotification().getBody();
+      //  String messageTitle = remoteMessage.getNotification().getTitle();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Bundle bundle = new Bundle();
+        bundle.putString("url", urlString);
+        intent.putExtras(bundle);
+        intent.putExtra("from", from);
+        intent.putExtra("title", messageTitle);
+        intent.putExtra("body", messageBody);
+        intent.putExtra("contents", messageBody+"11");
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, (int)(System.currentTimeMillis()/1000), intent, PendingIntent.FLAG_ONE_SHOT);
+        String channelId = "/topics/ALL";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(messageTitle)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelName = "/topics/ALL";
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify((int)(System.currentTimeMillis()/1000), notificationBuilder.build());
+
+
+
+
+
+
+
+
+        // 액티비티 쪽으로 메시지를 전달하는 메소드를 호출합니다. 메시지 받으면 바로 켜지도록
+      /*  sendToActivity(
                 getApplicationContext()
                 , remoteMessage.getFrom()
                 , remoteMessage.getNotification().getTitle()
@@ -48,15 +105,33 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 , remoteMessage.getData().toString()
         );
 
+        sendToActivity(
+                getApplicationContext()
+                , remoteMessage.getFrom()
+                , null
+                , null
+                , remoteMessage.getData().toString()
+        );*/
+
     }
 
 
 
     // Activity 쪽으로 메소드를 전달하는 메소드 입니다.
     private void sendToActivity(Context context, String from, String title, String body, String contents ){
-
-
+      /*  Log.d(TAG,
+                "title:" + remoteMessage1.getNotification().getTitle()
+                        + ", body:" + remoteMessage1.getNotification().getBody()
+                        + ", data:" + remoteMessage1.getData()
+        );*/
+        //Uri uri = Uri.parse(data);
+        //Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //startActivity(intent);
         Intent intent = new Intent(context, MainActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("url", urlString);
+        intent.putExtras(bundle);
         intent.putExtra("from", from);
         intent.putExtra("title", title);
         intent.putExtra("body", body);
@@ -65,7 +140,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         intent.addFlags(
                 Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP
-        );
+       );
 
 
         context.startActivity(intent);
