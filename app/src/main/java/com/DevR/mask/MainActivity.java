7a4c7bb1580946dcdbdd81ff0c1bc643e941fb0c
Changyu-Ryou <56837413+Changyu-Ryou@users.noreply.github.com>
2020-03-04 23:07:36 +0900

    ver 1.2
    웹뷰
    메인 연결
    애드몹 배너
    애드몹 전면광고
    새로고침 버튼
    Notification 연결 기능
    아이콘
    Playstore 제출

--- app/src/main/java/com/DevR/mask/MainActivity.java
+++ app/src/main/java/com/DevR/mask/MainActivity.java
@@ -1,29 +1,33 @@
 package com.DevR.mask;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.annotation.SuppressLint;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.os.Build;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -31,14 +35,22 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.RemoteMessage;
 
-import java.util.Map;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static com.DevR.mask.MyWebViewClient.currentpage;
 import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
 
+    static String currentPageUrl = "";
+    static String firstURL = "";
     static WebView mWebView; // 웹뷰 선언
     private WebSettings mWebSettings; //웹뷰세팅
     //static String from=MyFirebaseMessagingService.from;
@@ -47,14 +59,50 @@ public class MainActivity extends AppCompatActivity {
     //static Map<String, String> data=MyFirebaseMessagingService.data;
     private FirebaseAnalytics mFirebaseAnalytics;
     Intent intent;
+    String blogurl = "";
+    private AdView mAdView;     //하단배너광고
+    static Context context;
+    static SwipeRefreshLayout swipe;       //swipe reload
+    //SwipeRefreshLayout mySwipeRefreshLayout;
+    private InterstitialAd interstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        context = this;
         String id = "id";
         String name = "chan";
         intent = getIntent();
+
+        //하단 배너 init
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+        // 전면광고 초기화
+
+        interstitialAd = new InterstitialAd(context);
+        interstitialAd.setAdUnitId("ca-app-pub-7742126992195898/9239118392"); // * 자신의 전면광고 단위 아이디
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                interstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
+        });
+
         // 토큰이 등록되는 시점에 호출되는 메소드 입니다.
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,
 
@@ -98,26 +146,158 @@ public class MainActivity extends AppCompatActivity {
         // 웹뷰 시작
         mWebView = (WebView) findViewById(R.id.webView);
 
+
         mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
         mWebSettings = mWebView.getSettings(); //세부 세팅 등록
-        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
-        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
-        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
-        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
-        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
-        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
-        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
-        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
-        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
-        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스크립트 허용 여부
+        // mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        //  mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        //mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        // mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        //mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        //mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        //mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부  네트워크로 받아옴
+        //mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
 
-        System.out.println("url main="+intent.getStringExtra("url"));
-        if (intent.getStringExtra("url") == null)
-            mWebView.loadUrl("http://www.naver.net"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
-        else
+        MyWebViewClient testChromeClient = new MyWebViewClient();
+        mWebView.setWebViewClient(testChromeClient);
+        mWebView.setWebChromeClient(new WebChromeClient());//웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
+        // mWebView.setWebViewClient(new WebViewClientClass());//새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
+
+        mWebView.setWebChromeClient(new WebChromeClient() {         //웹뷰 내 팝업창 구현
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final android.webkit.JsResult result) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Pop up")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok,
+                                new AlertDialog.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        result.confirm();
+                                    }
+                                })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+                return true;
+            }
+        });
+        System.out.println("url main=" + intent.getStringExtra("url"));
+
+
+        if (intent.getStringExtra("url") == null) {
+            //mWebView.loadUrl("http://www.naver.net"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+            JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+            jsoupAsyncTask.execute();
+            //mWebView.loadUrl(blogurl); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+        } else
             mWebView.loadUrl(intent.getStringExtra("url"));
 
 
+        firstURL = mWebView.getUrl();
+        currentPageUrl = "firstpage";
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // 혹시 웹 뷰를 사용하는 액티비티일 경우 웹뷰 뒤로가기를 구현해준다.
+        System.out.println(mWebView.getUrl());
+        if (mWebView.canGoBack()) {
+            currentPageUrl = mWebView.getUrl();
+            mWebView.goBack();
+            System.out.println("can go");
+        } else {
+            //System.out.println("can not go");
+           // System.out.println(mWebView.getUrl() + "can not go" + firstURL);
+            /*
+            String nowURL = mWebView.getUrl() + "";
+
+            if(firstURL==null){
+                if(blogurl==""){
+                    JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                    jsoupAsyncTask.execute();
+                }
+                else {
+                    mWebView.loadUrl(blogurl);
+                }
+            } else if (!firstURL.equals(nowURL)) {
+               // System.out.println("안같다");
+                mWebView.loadUrl(blogurl);
+            } else
+                */
+            //if (mWebView.getUrl().equals(firstURL) && interstitialAd.isLoaded()) {
+
+                if (interstitialAd.isLoaded()) {
+              //  System.out.println("아니다");*/
+                interstitialAd.show();
+                interstitialAd.setAdListener(new AdListener() {
+                    @Override
+                    public void onAdClosed() {
+                        // 사용자가 광고를 닫으면 뒤로가기 이벤트를 발생시킨다.
+                        finish();
+                    }
+                });
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+    }
+
+/*
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {//뒤로가기 버튼 이벤트
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {//웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
+            mWebView.goBack();
+            return true;
+        }else{
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }*/
+
+    public void reflash(View view) {
+        mWebView.reload();
+    }
+
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String mainpageLink = "https://raw.githubusercontent.com/Changyu-Ryou/MaskStockClient/master/link.txt"; //git link.txt
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(mainpageLink).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("doc-" + doc);
+            Elements titles = doc.select("pre");
+            for (Element e : titles) {
+                System.out.println("title: " + e.text());
+                blogurl += e.text();
+            }
+            Element body = doc.body();
+            String aa = String.valueOf(body);
+            blogurl = Html.fromHtml(aa).toString();
+            System.out.println("url-" + blogurl);
+
+
+            return blogurl;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {     //텍스트를 어찌할건지 이후에 정함
+            mWebView.loadUrl(result);
+        }
     }
 
     // 서비스로 부터 인텐트를 받았을 때의 처리.
@@ -178,9 +358,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void btn(View view) {
-        finish();
-    }
 }
 
 
---
+++ app/src/main/java/com/DevR/mask/MyWebViewClient.java
@@ -0,0 +1,94 @@
+package com.DevR.mask;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.net.URISyntaxException;
+
+public class MyWebViewClient extends WebViewClient {
+    public static final String INTENT_URI_START = "intent:";
+    public static final String INTENT_FALLBACK_URL = "browser_fallback_url";
+    public static final String URI_SCHEME_MARKET = "market://details?id=";
+    public static String currentpage="";
+    public static View myView;
+    public static String mviewURL="";
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String uri) {
+        if (uri.toLowerCase().startsWith(INTENT_URI_START)) {
+            Intent parsedIntent = null;
+            try {
+                parsedIntent = Intent.parseUri(uri, 0);
+                MainActivity.context.startActivity(parsedIntent);
+            } catch(ActivityNotFoundException | URISyntaxException e) {
+                return doFallback(view, parsedIntent);
+            }
+        } else {
+            view.loadUrl(uri);
+        }
+
+        return true;
+    }
+
+
+    public boolean doFallback(WebView view, Intent parsedIntent) {
+
+        if (parsedIntent == null) {
+            return false;
+        }
+
+        String fallbackUrl = parsedIntent.getStringExtra(INTENT_FALLBACK_URL);
+        if (fallbackUrl != null) {
+            view.loadUrl(fallbackUrl);
+            return true;
+        }
+
+        String packageName = parsedIntent.getPackage();
+        if (packageName != null) {
+            MainActivity.context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(URI_SCHEME_MARKET + packageName)));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        mviewURL = view.getUrl();
+        String blank="";
+        if(MainActivity.firstURL==null){
+            MainActivity.firstURL=view.getUrl();
+        }
+        currentpage=view.getUrl();
+
+
+       /* String mainlink=MainActivity.firstURL;
+        System.out.println("view.getUrl()="+view.getUrl()+"\n");
+        System.out.println("currentpage="+currentpage+"\n");
+        System.out.println("MainActivity.currentPageUrl="+MainActivity.currentPageUrl+"\n");
+        System.out.println("mainlink="+mainlink+"\n");
+        System.out.println("MainActivity.firstURL="+MainActivity.firstURL+"\n\n\n");
+        System.out.println("\n  \n");*/
+
+        //if(!view.getUrl().equals(mainlink)&&view.getUrl().equals(currentpage)){
+         //   MainActivity.oneMoreBack();
+       // }
+
+
+        // WebView의 페이지 로드가 완료되면 콜백의 형태로 이 메쏘드가 호출됩니다..
+        // 좀 더 정확하게는 WebView가 이벤트 발생하는 경우 WebViewClient의 선언된 메쏘드들을 호출하고,
+        // 요 형태는 전형적인 옵저버 패턴의 모습입니다.
+
+
+        //분기햇을경우 이곳에서 처리해주면 될듯
+
+    }
+
+
+
+
+
+}
